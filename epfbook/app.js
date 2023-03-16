27253c56cca8b96c71936eddc4845b7c81ef6c4f
Trixtax-ops <tristan.michelena@epfedu.fr>
2023-03-16 16:09:22 +0100

    Student page

--- epfbook/app.js
+++ epfbook/app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// GET method route
+app.get('/student', function (req, res) {
+  res.send(["My name is Tristan and I try to understand the Get method", 
+  {name:"Tristan", school:"EPF"}]);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
