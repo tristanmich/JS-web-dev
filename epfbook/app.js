ae2b330a2c9220433917d57a213eac4a2f38fffa
Trixtax-ops <tristan.michelena@epfedu.fr>
2023-03-16 19:20:58 +0100

    Get request CSV

--- epfbook/app.js
+++ epfbook/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require("fs")
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +11,27 @@ app.get('/', (req, res) => {
 app.get('/student', function (req, res) {
   res.send(["My name is Tristan and I try to understand the Get method", 
   {name:"Tristan", school:"EPF"}]);
+});
+
+// GET method route
+app.get('/csv', function (req, res) {
+  fs.readFile('Sheet_school.csv', 'utf8', (err, data) => {
+    if (err) throw err; {
+      console.log(data);
+  }
+  var lines = data.split('\n');
+  const header = lines[0].split(',');
+  var list = [];
+  for(let i = 1; i < lines.length-1; i++) {
+    var values = lines[i].split(',');
+    var dict = {};
+    dict[header[0]] = values[0].replace('\\n', '');
+    dict[header[1]] = values[1];
+    list.push(dict);
+  }
+  res.set('Content-Type', 'text/html');
+  res.send(list);
+  });
 });
 
 app.listen(port, () => {
